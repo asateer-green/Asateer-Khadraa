535a4d840c0984570eb52199f3bb9724c2973916
Raghda Mohammed <raghda191987@gmail.com>
2026-07-14 21:50:46 +0300

    Fix: remove base path from vite config for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Asateer-Khadraa/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
